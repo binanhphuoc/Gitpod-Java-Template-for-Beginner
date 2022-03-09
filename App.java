6781b7fb5cfd6970b9cc67d06d3f3eb467ac7c59
Phuoc Do <binanhphuoc@gmail.com>
2022-03-09 17:53:02 +0000

    Update to hello world

--- App.java
+++ App.java
@@ -1,17 +1,8 @@
 /**
  * App
  */
-
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        for (int i = 0; i < 11; i++) {
-            System.out.println(i);
-        }
-
-        Scanner inputStream = new Scanner(System.in);
-        String name = inputStream.nextLine();
-        System.out.println(name);
+        System.out.println("Hello World!");
     }
 }
