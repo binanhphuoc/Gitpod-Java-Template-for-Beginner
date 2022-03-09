f21444dbff6d32b9eccdbaa8a3c4930d7f25944f
Phuoc Do <binanhphuoc@gmail.com>
2022-03-09 17:41:37 +0000

    Init

--- App.java
+++ App.java
@@ -1,11 +1,17 @@
 /**
  * App
  */
-public class App {
 
+import java.util.Scanner;
+
+public class App {
     public static void main(String[] args) {
         for (int i = 0; i < 11; i++) {
             System.out.println(i);
         }
+
+        Scanner inputStream = new Scanner(System.in);
+        String name = inputStream.nextLine();
+        System.out.println(name);
     }
 }
